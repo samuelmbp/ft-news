ba2579df252d9f660080adcd4f85780a8527e2fa
Samuel Raducan <samuel.thembp@gmail.com>
2022-05-27 08:02:25 +0100

    Setup the path to the views folder and the view engine as ejs

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-// Middleware helpers
-app.use(express.json());
+const newsRouter = require('./routes/newsRouter');
 
-const port = process.env.PORT || 3000;
+/** Middleware helpers */
+app.use(express.json());
+app.use(cors());
+app.use('/', newsRouter);
+
+/** Template Engine */
+app.set('views', path.join(__dirname, './views')); //eslint-disable-line
+app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 3000; //eslint-disable-line
 app.listen(port, () => {
 	console.log(`Listening on port ${port}...`);
 });
